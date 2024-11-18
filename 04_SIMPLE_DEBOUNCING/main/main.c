9a12dca91443456a942c4199112a44eb3ffd0398
jeromemoses <jeromemoses05@gmail.com>
2024-11-18 16:01:30 +0530

    Simple debouncing (firmware) test

---
+++ 04_SIMPLE_DEBOUNCING/main/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include "esp_attr.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/gpio.h"
+#include "hal/gpio_types.h"
+#include "portmacro.h"
+
+//Input pin definition
+#define IN_PIN 16
+
+//setting a interrupt queue
+QueueHandle_t interruptQueue;
+
+static void IRAM_ATTR gpio_isr_handler(void *args)
+{
+	int pinNumber = (int)args;
+	xQueueSendFromISR(interruptQueue, &pinNumber, NULL);
+}
+
+void buttonPushedTask (void *parmas)
+{
+	int pinNumber, count = 0;
+	while (true) 
+	{
+		if (xQueueReceive(interruptQueue, &pinNumber, portMAX_DELAY)) 
+		{	
+			//disables the interrupt by removing the pin from ISR
+			gpio_isr_handler_remove(IN_PIN);
+			
+			//wait and check for the button to be released
+			do {
+					vTaskDelay(20 / portTICK_PERIOD_MS);
+				}while (gpio_get_level(IN_PIN) == 1);
+			
+			//print the states on serial
+			printf("GPIO %d is pressed %d times, pin state %d\n",pinNumber, count++, gpio_get_level(IN_PIN));	
+			
+			//re enable the interrupt by adding the gpio into ISR (Interrupt Service Routine)
+			gpio_isr_handler_add(IN_PIN, gpio_isr_handler, (void *) pinNumber);				
+		}
+	}
+}
+
+void app_main(void)
+{
+	//gpio configuration
+	gpio_set_direction(IN_PIN, GPIO_MODE_INPUT);
+	gpio_pulldown_en(IN_PIN);
+	gpio_set_pull_mode(IN_PIN, GPIO_PULLDOWN_ENABLE);
+	gpio_pullup_dis(IN_PIN);
+	gpio_set_intr_type(IN_PIN, GPIO_INTR_POSEDGE);
+	
+	
+	//declaring size for the interrupt Queue
+	interruptQueue = xQueueCreate(10, sizeof(int));
+	xTaskCreate(buttonPushedTask, "buttonPushedTask", 2048, NULL, 1, NULL);
+	
+	//ISR configuration for GPIO interrupt
+	gpio_install_isr_service(0);
+	gpio_isr_handler_add(IN_PIN, gpio_isr_handler, (void *)IN_PIN);
+	
+    while (true) {
+        printf("RTOS test, Hi from app_main (^._.^) \n");
+        sleep(2);
+    }
+}
